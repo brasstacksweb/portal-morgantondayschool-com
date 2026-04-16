f89a182d91bfbdef3f524ef7027530961cb12314
Trevor Plassman <trevor@brasstacksweb.com>
2026-04-16 15:54:39 +0000

    hijack emails in devmode to only send to dev email

--- modules/site/src/SiteModule.php
+++ modules/site/src/SiteModule.php
@@ -3,9 +3,12 @@
 namespace modules\site;
 
 use craft\events\RegisterUrlRulesEvent;
+use craft\helpers\App;
+use craft\mail\Mailer;
 use craft\web\UrlManager;
 use yii\base\Event;
 use yii\base\Module;
+use yii\mail\MailEvent;
 
 class SiteModule extends Module
 {
@@ -20,5 +23,38 @@ class SiteModule extends Module
             $event->rules['cmd/site/test'] = 'site-module/sitemap/test';
             $event->rules['site.webmanifest'] = 'site-module/sitemap/manifest';
         });
+
+        // Override email address for all messages in development
+        Event::on(
+            Mailer::class,
+            Mailer::EVENT_BEFORE_PREP,
+            function (MailEvent $event) {
+                if (!App::devMode()) {
+                    return;
+                }
+
+                $devEmail = App::env('DEV_EMAIL');
+
+                if (!$devEmail) {
+                    // If the .env variable isn't set, prevent the email from sending just in case
+                    $event->isValid = false;
+                    \Craft::dd('DEV_EMAIL environment variable not set. Email sending prevented.');
+
+                    return;
+                }
+
+                // For debugging, add the original recipients to the subject
+                $to = is_array($event->message->getTo())
+                    ? implode(', ', array_keys($event->message->getTo()))
+                    : $event->message->getTo();
+
+                $subject = '[TEST to '.$to.'] '.$event->message->getSubject();
+
+                $event->message->setTo($devEmail);
+                $event->message->setCc(null);
+                $event->message->setBcc(null);
+                $event->message->setSubject($subject);
+            }
+        );
     }
 }
